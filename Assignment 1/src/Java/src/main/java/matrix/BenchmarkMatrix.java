3f212e767bb0259e243a41e62d1a4f5b638f6801
radoslawazukowska <radoslawa.zuk@gmail.com>
2024-10-20 21:29:04 +0100

    fixed benchmark code in java

--- Assignment 1/src/Java/src/main/java/matrix/BenchmarkMatrix.java
+++ Assignment 1/src/Java/src/main/java/matrix/BenchmarkMatrix.java
@@ -3,16 +3,17 @@ package matrix;
 import java.util.Random;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
-
 @State(Scope.Benchmark)
 public class BenchmarkMatrix {
-    @Param({"10", "100"})
+    @Param({"100", "200", "300", "400", "500", "600", "700", "800", "900", "1000"})
     private int size;
 
     private double [][] a;
@@ -33,6 +34,7 @@ public class BenchmarkMatrix {
     }
 
     @Benchmark
+    @BenchmarkMode(Mode.SampleTime)
     public void testMatrixMultiplication() {
         Matrix.multiply(a, b);
     }
