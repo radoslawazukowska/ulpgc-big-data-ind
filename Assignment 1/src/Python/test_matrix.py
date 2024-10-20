29333658c8699dd909c0639bdd30291a8aae2308
radoslawazukowska <radoslawa.zuk@gmail.com>
2024-10-20 20:54:22 +0100

    updated python test

--- Assignment 1/src/Python/test_matrix.py
+++ Assignment 1/src/Python/test_matrix.py
@@ -8,7 +8,7 @@ def generate_random_matrix(n):
     return [[random() for _ in range(n)] for _ in range(n)]
 
 
-@pytest.mark.parametrize("size", [16, 32, 64, 128, 256, 512, 1024])
+@pytest.mark.parametrize("size", [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000])
 def test_matrix_multiplication(benchmark, size):
     A = generate_random_matrix(size)
     B = generate_random_matrix(size)
