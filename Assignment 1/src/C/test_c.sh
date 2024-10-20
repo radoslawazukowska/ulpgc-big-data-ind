78066643bcbe935f2669a270c200a18440917d16
radoslawazukowska <radoslawa.zuk@gmail.com>
2024-10-20 20:53:45 +0100

    updated bash script

--- Assignment 1/src/C/test_c.sh
+++ Assignment 1/src/C/test_c.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sizes=(16 32 64 128 256 512 1024)
+sizes=(100 200 300 400 500 600 700 800 900 1000)
 
-for size in "${sizes[@]}"
-do
-    perf stat -o ../../results/benchmark-results-c/perf_output_$size.txt ./matrix_multiplication $size
+for size in "${sizes[@]}"; do
+    mkdir ../../results/benchmark-results-c/$size
+    for i in {1..10}; do
+        perf stat -o "../../results/benchmark-results-c/$size/perf_output_sample$i.txt" ./matrix_multiplication $size
+    done
 done
